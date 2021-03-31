6582c9c959ece453e95e3da83e8827d7e2caf7e3
xavierdidelot <xavier.didelot@gmail.com>
2021-03-31 18:16:48 +0100

    First semi-parametric test

--- R/test.R
+++ R/test.R
@@ -9,7 +9,28 @@ test0=function(dates,coalints,Ne)
   h=hist(coalints,main='',breaks=20)
   br=h$breaks
   lines(br[-1]-diff(br)[1]/2,length(coalints)*(pexp(br[-1],1/Ne)-pexp(br[-length(br)],1/Ne)))
-  pvals=pexp(coalints,1/Ne)
+  pvals=1-pexp(coalints,1/Ne)
+  pvals=p.adjust(pvals,"fdr")
+  return(pvals)
+}
+
+#' Semi-parametric test for the presence of imports
+#' @param dates Dates of samples
+#' @param coalints Coalescent intervals
+#' @param epsilon Precision parameter
+#' @return p-values for importation
+#' @export
+test1=function(dates,coalints,epsilon)
+{
+  l=length(dates)
+  NeHat=rep(NA,l)
+  for (i in 1:l) {
+    w=setdiff(which(abs(dates-dates[i])<epsilon),i)
+    NeHat[i]=median(coalints[w])/log(2)
+  }
+  plot(dates,coalints,xlab='',ylab='')
+  lines(dates,NeHat,col='red')
+  pvals=1-pexp(coalints,1/NeHat)
   pvals=p.adjust(pvals,"fdr")
   return(pvals)
 }
