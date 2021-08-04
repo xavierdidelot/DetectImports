0f65ab0e5dffa30d29168db681a42a5e5ca31cc2
David <David.Helekal@warwick.ac.uk>
2021-08-04 14:33:09 +0100

    try projected gps

--- run/run_stan.R
+++ run/run_stan.R
@@ -19,7 +19,7 @@ compute_ci <- function(x, conf=0.95) {
   return(ci)
 }
 
-ntip<-500
+ntip<-120
 set.seed(123)
 tree<-simImports(localPopStart=2020,importDates=c(2020.25,2020.5),
                 samplingStartDate=2020,samplingEndDate=2021,samplingNumber=ntip)
@@ -43,26 +43,30 @@ cnames <- sapply(c(1:length(coalints)),function(i) paste0(
   "f[",i,"]"))
 
 mod <- cmdstan_model(paste0("../stan/gpmodel.stan"))
-data_list <- list(N = length(coalints),intervals=coalints, T_s=dates, shape=5, scale=5)
+data_list <- list(N = length(coalints), intervals=coalints, T_s=dates, shape=5, scale=5)
 fit <- mod$sample(
   data = data_list,
   seed = 123,
+  adapt_delta=0.9,
   chains = 4,
-  parallel_chains = 40
+  parallel_chains = 4,
   refresh = 500, 
-  iter_sampling = 1e3
+  iter_sampling = 3e3,
+  iter_warmup = 1e3
 )
 
 draws_array <- fit$draws()
 draws_df <- as_draws_df(draws_array)
 
-c_means <- draws_df[cnames]
+probs <- draws_df[cnames]
+e_probs <- apply(probs,2,mean)
 
-ci_lo <- apply(c_means, 2, function(x) compute_ci(x, conf=0.98)[1])
-ci_hi <- apply(c_means, 2, function(x) compute_ci(x, conf=0.98)[2])
+#ci_lo <- apply(c_means, 2, function(x) compute_ci(x, conf=0.98)[1])
+#ci_hi <- apply(c_means, 2, function(x) compute_ci(x, conf=0.98)[2])
 
-data_df <- data.frame(x=dates, hi=ci_hi, lo=ci_lo, int=coalints, imp=imp)
+#data_df <- data.frame(x=dates, hi=ci_hi, lo=ci_lo, int=coalints, imp=imp)
+data_df <- data.frame(x=dates, prob=e_probs, int=coalints, imp=imp)
 
-p <- ggplot(data_df, aes(x)) + geom_ribbon(aes(ymin=lo, ymax=hi)) + geom_point(aes(y=int,color=imp))
+p <- ggplot(data_df, aes(x)) + geom_point(aes(y=int,shape=imp, color=prob))
 plot(p)
 dev.off()
