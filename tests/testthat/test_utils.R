7bd6ccfd4217e39843184020b2ecfdc99f9d0447
xavierdidelot <xavier.didelot@gmail.com>
2021-09-14 16:29:47 +0200

    Code for paper examples

--- tests/testthat/test_utils.R
+++ tests/testthat/test_utils.R
@@ -4,11 +4,11 @@ context("Test utility functions")
 test_that("Utility functions work as expected.", {
   set.seed(0)
   NeFun=function(t){return(5)}
-  tree=simCoal(seq(2000,2020,0.1),NeFun)
+  tree=simCoal(seq(2000,2020,1),NeFun)
   expect_silent(plotImports(tree,c(2,3)))
   expect_silent(plotCoalInt(tree))
   expect_silent(plotBoth(tree,NeFun))
-  res=detectImports(tree,verbose=F)
+  res=detectImports(tree,verbose=F,iter=100,nchains=1)
   expect_is(capture_output(print(res)),'character')
   expect_is(capture_output(summary(res)),'character')
   expect_silent(plot(res))
