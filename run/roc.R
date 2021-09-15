8690123ff093fa138a83b51df6005b96c3223633
xavierdidelot <xavier.didelot@gmail.com>
2021-09-15 10:27:15 +0200

    ROC figure

---
+++ run/roc.R
@@ -0,0 +1,47 @@
+#This is a ROC curve comparison between methods
+rm(list=ls())
+library(DetectImports)
+library(ape)
+
+methods=c('full','constant','fast')
+set.seed(0)
+ntip=500
+pvalres=10000
+roc=array(0,dim=c(pvalres,2,length(methods)))
+repeats=100
+for (i in 1:repeats) {
+  print(i)
+  set.seed(i)
+  tree=simImports(localPopStart=2020,importDates=c(2020.25,2020.5),
+                  samplingStartDate=2020,samplingEndDate=2021,samplingNumber=ntip)
+  real=tree$imports#real imports
+  unreal=setdiff(1:ntip,real)
+  for (m in 1:length(methods)) {
+    if (methods[m]=='full') pvals=detectImports(tree,verbose=F,seed=0)$pvals
+    if (methods[m]=='constant') pvals=detectImports(tree,verbose=F,seed=0,constant=T)$pvals
+    if (methods[m]=='fast') pvals=detectImportsFAST(tree)$pvals
+    for (j in 1:pvalres) {
+      p=j/pvalres
+      roc[j,1,m]=roc[j,1,m]+length(which(pvals[  real]<p))/length(real)
+      roc[j,2,m]=roc[j,2,m]+length(which(pvals[unreal]<p))/length(unreal)
+    }
+  }
+}
+roc=roc/repeats
+save.image('run/roc.RData')
+
+for (p in c(0.05,0.01,0.001)) for (m in 1:length(methods))
+  print(sprintf('With cutoff p=%.3f, %s method has sensitivity=%.2f and specificity=%.2f',p,methods[m],roc[pvalres*p,1,m],1-roc[pvalres*p,2,m]))
+
+#Plotting
+methods=c('Full model','Constant model')
+cols=c('red','blue')
+pdf('/tmp/roc.pdf',7,7)
+plot(c(0,1),c(0,1),type='l',lty=2,xlab ='False Positive Rate (1-Specificity)',ylab='True Positive Rate (Sensitivity)')
+for (m in 1:length(methods)) {
+  lines(c(0,roc[,2,m],1),c(0,roc[,1,m],1),col=cols[m])
+  #points(roc[pvalres*c(0.01,0.05),2,m],roc[pvalres*c(0.01,0.05),1,m],col=cols[m])
+}
+legend('bottomright',legend=methods,col=cols, lty=1, cex=0.8)
+dev.off()
+system('open /tmp/roc.pdf')
