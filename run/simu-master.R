ca985164aeea356deba74c33049c6fcafe0cc9b9
xavierdidelot <xavier.didelot@gmail.com>
2022-01-18 17:46:27 +0000

    Migrations both ways

--- run/simu-master.R
+++ run/simu-master.R
@@ -1,7 +1,24 @@
 rm(list=ls())
 
+library(ape)
 library(treeio)
 library(DetectImports)
+
+countMigs=function(phy,lo)
+{
+  parents=c()
+  parents[phy$edge[,2]]=phy$edge[,1]
+  n=Ntip(phy)
+  w=rep(NA,n)
+  for (i in 1:n) {
+    k=i
+    while (!is.na(k) && lo[k]==0) k=parents[k]
+    if (is.na(k)) k=0
+    w[i]=k
+  }
+  return(length(unique(w))-1)
+}
+
 
 res=c()
 for (rep in 1:100) {
@@ -12,8 +29,8 @@ for (rep in 1:100) {
   system('cp start.xml master.xml')
   localcoalrate=1
   globalcoalrate=0.1
-  migrate1=runif(1,min=0,max=0.5)
-  migrate2=0#keep this equal to zero to have no migration from local to global
+  migrate1=runif(1,min=0,max=0.5)#backward in time migration from local to global
+  migrate2=migrate1#backward in time migration from global to local
   system(sprintf('perl -i -wpe"s/localcoalrate/%f/g" master.xml',localcoalrate))
   system(sprintf('perl -i -wpe"s/globalcoalrate/%f/g" master.xml',globalcoalrate))
   system(sprintf('perl -i -wpe"s/migrate1/%f/g" master.xml',migrate1))
@@ -37,13 +54,15 @@ for (rep in 1:100) {
   #axisPhylo(1)
   lo=as.numeric(t@data$location)
   lo[as.numeric(t@data$node)]=lo
-  mi=sum(lo[phy$edge[,1]]!=lo[phy$edge[,2]])
+  #mi=sum(lo[phy$edge[,1]]==1 & lo[phy$edge[,2]]==0)
+  mi=countMigs(phy,lo)
 
   #Detect imports and compare with number of migrations
+  phy2=collapse.singles(phy)
   set.seed(rep)
-  r=detectImports(phy,verbose = F)
+  r=detectImports(phy2,verbose = F)
   set.seed(rep)
-  c=detectImports(phy,verbose = F,constant=T)
+  c=detectImports(phy2,verbose = F,constant=T)
   res=rbind(res,(c(mi,length(which(r$pvals<0.05)),length(which(r$pvals<0.01)),length(which(r$pvals<0.001)),
                       length(which(c$pvals<0.05)),length(which(c$pvals<0.01)),length(which(c$pvals<0.001)))))
 
