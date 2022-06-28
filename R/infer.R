d793cccd684a63a57b6f422306662076ead0d8a8
David <2306427+dhelekal@users.noreply.github.com>
2022-06-28 11:48:48 +0100

    Add v2 model, epsilon contam model
    Change bool 'constant' to  flag 'model'

--- R/infer.R
+++ R/infer.R
@@ -118,7 +118,7 @@ detectImportsESD=function(tree,epsilon,alpha=0.05,maxi=round(Ntip(tree)/10),show
 
 #' Bayesian test for the presence of imports
 #' @param tree Tree
-#' @param constant Whether to assume that the local population size is constant
+#' @param model One of 'constant' 'gp' 'gpcontam'
 #' @param adjust Method for adjusting p-values (default is none)
 #' @param nchains Number of chains to run in parallel
 #' @param verbose Whether to produce verbose output
@@ -127,7 +127,7 @@ detectImportsESD=function(tree,epsilon,alpha=0.05,maxi=round(Ntip(tree)/10),show
 #' @param seed Seed
 #' @return Results of importation test
 #' @export
-detectImports=function(tree,constant=FALSE,adjust='none',verbose=T,diagnostics=F,nchains=4,iter=4000,seed=NULL)
+detectImports=function(tree,model='gp',adjust='none',verbose=T,diagnostics=F,nchains=4,iter=4000,seed=NULL)
 {
   if (is.null(tree$stats)) tree=keyStats(tree)
   m=tree$stats
@@ -136,25 +136,31 @@ detectImports=function(tree,constant=FALSE,adjust='none',verbose=T,diagnostics=F
   dates<-m[toana,'dates']
   coalints<-m[toana,'coalint']
 
-  if (constant) {
+  if (model == 'constant') {
     mod <- cmdstan_model(system.file('stan','constant.stan',package='DetectImports',mustWork = T),compile=F)
     data_list <- list(N = length(coalints), intervals=coalints, T_s=dates)
     coalnames <- rep("coal_mean",length(coalints))
   }
-  else
+  else if(model == 'gp')
   {
-    mod <- cmdstan_model(system.file('stan','gpmodel.stan',package='DetectImports',mustWork = T),compile=F)
+    mod <- cmdstan_model(system.file('stan','gpmodel_v2.stan',package='DetectImports',mustWork = T),compile=F)
     data_list <- list(N = length(coalints), intervals=coalints, T_s=dates, M=20, c=2.0)
     coalnames <- sapply(c(1:length(coalints)),function(i) paste0("coal_means[",i,"]"))
+  } else if (model== 'gpcontam') {
+    mod <- cmdstan_model(system.file('stan','gpmodel_v2_contam.stan',package='DetectImports',mustWork = T),compile=F)
+    data_list <- list(N = length(coalints), intervals=coalints, T_s=dates, M=20, c=2.0)
+    coalnames <- sapply(c(1:length(coalints)),function(i) paste0("coal_means[",i,"]"))
+  } else{
+    stop(paste("Unrecognised option:",model))
   }
 
   if (verbose) {
     mod$compile()
-    fit <- mod$sample(data = data_list,adapt_delta=0.9,chains = nchains,parallel_chains = nchains,refresh = round(iter*0.1),iter_sampling = round(iter*0.75),iter_warmup = round(iter*0.25),seed=seed)
+    fit <- mod$sample(data = data_list,adapt_delta=0.95, chains = nchains,parallel_chains = nchains,refresh = round(iter*0.1),iter_sampling = round(iter*0.75),iter_warmup = round(iter*0.25),seed=seed)
   } else {
     invisible(capture.output(suppressMessages({
       mod$compile()
-      fit <- mod$sample(data = data_list,adapt_delta=0.9,chains = nchains,parallel_chains = nchains,refresh = 0,show_messages=F,iter_sampling = round(iter*0.75),iter_warmup = round(iter*0.25),seed=seed)
+      fit <- mod$sample(data = data_list,adapt_delta=0.95, chains = nchains,parallel_chains = nchains,refresh = 0,show_messages=F,iter_sampling = round(iter*0.75),iter_warmup = round(iter*0.25),seed=seed)
     })))
   }
 
---
+++ inst/stan/gpmodel_v2.stan
@@ -0,0 +1,63 @@
+functions {
+   //eigenfunction
+   vector precompute_basis(vector T, real L, int j) {
+       vector[size(T)] basis;
+       real lambda = j*pi()/(2*L);
+       basis = 1/sqrt(L) * sin(lambda*(T+L));
+       return (basis);
+   }
+
+   //Spectral density function associated with Matern kernel with nu=3/2, dimension D=1 and Euclidiean distance
+   real spec_dens_matern(real x, real alpha, real l) {
+       real dens = 4 * (alpha^2) * (sqrt(3)/l)^3 * 1/((sqrt(3)/l)^2 + x^2)^2;
+       return(dens);
+   }
+}
+
+data {
+    int<lower = 1> N; // sample number
+    real<lower = 0> intervals[N]; // coalescent intervals
+    vector[N] T_s; // sampling times
+    int<lower=1> M;// number of basis functions
+    real<lower=1> c;// boundary
+}
+
+transformed data {
+    // transform data to have domain[-1,1]
+    vector[N] T_centered = (to_vector(T_s) - min(T_s));
+    T_centered = T_centered - (max(T_centered)/2);
+    real S=max(T_centered);
+    T_centered = T_centered / S;
+    real L = c;
+    matrix[N, M] basis;
+    for (idx in 1:M) {
+        basis[:, idx] = precompute_basis(T_centered, L, idx);
+    }
+}
+
+parameters {
+    vector[M] f_tilde; // weights
+    real<lower = 0> alpha_tilde;  // kernel magnitude (ie marginal variance)
+    real<lower = 0> l; // kernel length scale
+}
+
+transformed parameters {
+    real alpha = 0.5*alpha_tilde;
+    vector[N] a_coeffs;
+    vector[N] coal_means;
+    vector[M] spec_dens;
+    {
+        for(idx in 1:M) {
+            spec_dens[idx] = sqrt(spec_dens_matern(idx*pi()/(2*L), alpha, l));
+        }
+        a_coeffs = (basis)*(spec_dens.*f_tilde);
+    }
+    coal_means = exp(a_coeffs);
+}
+
+model {
+    alpha_tilde ~ normal(0,12);
+    l ~ inv_gamma(5,5);
+    f_tilde ~ normal(0,1);
+    intervals ~ exponential(coal_means);
+}
