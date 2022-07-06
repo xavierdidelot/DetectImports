8b1c316905ec1804a4f9c456b6d66a4b5a014b05
xavierdidelot <xavier.didelot@gmail.com>
2022-07-06 17:25:20 +0100

    Facilitate access to tree plot

--- R/utils.R
+++ R/utils.R
@@ -40,31 +40,37 @@ plotImports=function(tree,imports,showTimeAxis=T,...)
 
 #' Plotting method for DetectImports results
 #' @param x Output from DetectImports test methods
+#' @param type Type of plot to produce. Can be 'scatter' (default) or 'tree'
 #' @param ... Additional parameters are passed on
 #' @return Plot of DetectImports results
 #' @export
-plot.resDetectImports=function(x,...)
+plot.resDetectImports=function(x,type='scatter',...)
 {
   stopifnot(inherits(x, "resDetectImports"))
-  n=Ntip(x$tree)
-  stats=x$tree$stats
-  dates=stats[1:n,'dates']
-  symbols=rep(1,length(dates))
-  cols=rep(1,length(dates))
-  cols[which(x$pvals<=0.01 )]=2
-  cols[which(x$pvals<=0.001)]=3
-  cols=c('black','red3','red')[cols]
-  if (!is.null(x$tree$imports)) symbols[x$tree$imports]=2
-  args=list(x=dates,y=stats[1:n,'coalint'],pch=symbols,col=cols)
-  args=modifyList(args,list(...))
-  if (!hasArg('xlab')) args=modifyList(args,list(xlab='Sampling dates'))
-  if (!hasArg('ylab')) args=modifyList(args,list(ylab='Coalescent intervals'))
-  do.call("plot",args)
-  ix=sort(dates,index.return=T)$ix
-  if (!is.null(x$mus)) lines(dates[ix],x$mus[ix],col='blue')
-  if (!is.null(x$mus_low )) lines(dates[ix],x$mus_low [ix],col='blue',lty=2)
-  if (!is.null(x$mus_high)) lines(dates[ix],x$mus_high[ix],col='blue',lty=2)
-  legend('topleft',legend=c("p>0.01","0.001<p<=0.01","p<=0.001"),col=c("black","red3","red"), lty=1, cex=0.8)
+  if (type=='scatter') {
+    n=Ntip(x$tree)
+    stats=x$tree$stats
+    dates=stats[1:n,'dates']
+    symbols=rep(1,length(dates))
+    cols=rep(1,length(dates))
+    cols[which(x$pvals<=0.01 )]=2
+    cols[which(x$pvals<=0.001)]=3
+    cols=c('black','red3','red')[cols]
+    if (!is.null(x$tree$imports)) symbols[x$tree$imports]=2
+    args=list(x=dates,y=stats[1:n,'coalint'],pch=symbols,col=cols)
+    args=modifyList(args,list(...))
+    if (!hasArg('xlab')) args=modifyList(args,list(xlab='Sampling dates'))
+    if (!hasArg('ylab')) args=modifyList(args,list(ylab='Coalescent intervals'))
+    do.call("plot",args)
+    ix=sort(dates,index.return=T)$ix
+    if (!is.null(x$mus)) lines(dates[ix],x$mus[ix],col='blue')
+    if (!is.null(x$mus_low )) lines(dates[ix],x$mus_low [ix],col='blue',lty=2)
+    if (!is.null(x$mus_high)) lines(dates[ix],x$mus_high[ix],col='blue',lty=2)
+    legend('topleft',legend=c("p>0.01","0.001<p<=0.01","p<=0.001"),col=c("black","red3","red"), lty=1, cex=0.8)
+  }
+  if (type=='tree') {
+    plotImports(x$tree,which(x$pvals<0.01))
+  }
 }
 
 #' Print function for DetectImports results
